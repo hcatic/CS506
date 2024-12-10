1cbcfb1ced97bdd7ca7fa4ea351fb624aeeaeb50
Brandon325 <46274399+Brandon325@users.noreply.github.com>
2024-12-09 22:18:35 -0500

    Update structure.sh

--- structure.sh
+++ structure.sh
@@ -1,13 +1,20 @@
-project_root/
-├─ data/
-│  ├─ Airbnb_Open_Data.csv
-├─ notebooks/
-│  ├─ analysis.ipynb
-├─ src/
-│  ├─ data_loading.py
-│  ├─ preprocessing.py
-│  ├─ eda.py
-│  ├─ feature_engineering.py
-│  ├─ clustering.py
-│  ├─ pca_analysis.py
-│  ├─ modeling.py
+project-root/
+│
+├── data/ # Raw and processed data
+│ ├── raw/ # Raw data files (e.g., CSVs, JSONs)
+│ ├── processed/ # Processed data ready for analysis
+├── models/ # Saved models
+│ ├── pkl files/ # Serialized trained models (e.g., .pkl files)
+├── notebooks/ # Source code
+│ ├── eda.ipynb # Feature engineering functions
+│ ├── preprocessing.ipynb # Data cleaning and preprocessing functions
+│ ├── feature_engineering.ipynb # Feature engineering functions
+│ └── modeleling.ipynb # Model training script
+│ ├── visualsations.ipynb # Feature engineering functions
+├── visualizations/ # Visualization files
+│ ├── .pngs # Saved plots and visualizations
+├── website/
+├─ |─ website files/ ...
+├── README.md # Project documentation
+├── requirements.txt # Python dependencies
+└── .gitignore
